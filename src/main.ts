1b2c6257c83516ae2f0fdf010fb5552bf175bae8
Karolina Modes <aedriell@gmail.com>
2022-10-21 02:53:40 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn'],
   });
+  app.enableCors();
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
